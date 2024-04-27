f6d196f3546e7c7c1d5f126111ca84f3e531d94c
Bressendorff <frederik@bressendorff.eu>
2024-04-27 19:35:39 +0200

    Rebuild app in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,95 @@
-import { createMemo, createSignal, type Component } from 'solid-js';
-import { Input, InputAdornment, TextField } from '@suid/material';
+import { useRef, useState } from "react";
+import { Center, Grid, GridItem, HStack, Input, Text } from "@chakra-ui/react";
 
-const App: Component = () => {
-  const [salary, setSalary] = createSignal<number>(0);
-  const salaryInput = createMemo<HTMLInputElement | null>(() => null);
+interface SalaryQuery {
+  salary: number;
+}
 
-  const handleSalaryChange = (e: Event) => {
-    const target = e.target as HTMLInputElement;
-    const salaryBeforeTax = parseFloat(target.value)
-    const salaryAfterTax = salaryBeforeTax * 0.92 * 0.6299;
-    setSalary(salaryAfterTax);
+function App() {
+  const [salaryQuery, setSalaryQuery] = useState<SalaryQuery>();
+
+  const ref = useRef<HTMLInputElement>(null);
+
+  const salaryInputChangedHandler = (salary: string | undefined) => {
+    if (!salary) return;
+
+    const salaryBeforeTax = parseInt(salary);
+    const salaryAfterTax = calculateSalaryAfterTax(salaryBeforeTax);
+    setSalaryQuery({ ...salaryQuery, salary: salaryAfterTax });
+  };
+
+  const calculateSalaryAfterTax = (salary: number): number => {
+    //Tax rates
+    const AMcontributionPercentage = 8;
+    const roskildeTax = 37.21;
+    const ATPcontributionDKK = 99;
+
+    //Tax deductions
+    const personalDeduction = 49700 / 12;
+    const employmentDeduction = salary * 0.1065;
+
+    const deductions = personalDeduction + employmentDeduction;
+
+    if (salary > 10000) {
+      salary -= ATPcontributionDKK;
+    }
+
+    salary *= (100 - AMcontributionPercentage) / 100;
+    salary -= deductions;
+
+    if (salary > 0) {
+      salary *= (100 - roskildeTax) / 100;
+    }
+
+    salary += deductions;
+
+    return Math.round(salary);
   };
 
   return (
-  <div style="padding:1em">
-    <div class="grid w-full max-w-sm items-center gap-1.5">
-      <Input type="number" id="salary" placeholder="Monthly Salary (DKK)" ref={salaryInput} oninput={(e) => handleSalaryChange(e)} />
-    </div>
+    <>
+      <Center>
+        <Text fontSize={"5xl"}>Salary Calculator</Text>
+      </Center>
+      <Center>
+        <Grid
+          templateAreas={`"header header"
+        "main main"`}
+          gridTemplateRows={"50px 1fr 30px"}
+          gridTemplateColumns={"150px 1fr"}
+          h="200px"
+          gap="1"
+          color="blackAlpha.700"
+          fontWeight="bold"
+        >
+          <GridItem pl="2" area={"header"}>
+            <Input
+              ref={ref}
+              borderRadius={20}
+              placeholder="Enter salary"
+              variant="filled"
+              onInput={() => salaryInputChangedHandler(ref.current?.value)}
+              type="number"
+            />
+          </GridItem>
+          <GridItem pl="2" area={"main"}>
+            <HStack>
+              <Text textColor={"white"}>Salary after tax</Text>
 
-    <br/>
-
-
-    <div class="grid w-full max-w-sm items-center gap-1.5">
-    <TextField
-        id="outlined-basic"
-        label="After tax"
-        variant="outlined"
-        disabled={true}
-        value={salary()}
-        InputProps={{
-          endAdornment: <InputAdornment position='start'>DKK</InputAdornment>,
-        }}
-        
-      ></TextField>
-    </div>
-  </div>)
-};
+              <Text
+                textColor={"white"}
+                borderColor={"black"}
+                borderWidth={"2px"}
+                padding={1}
+              >
+                {salaryQuery?.salary ? salaryQuery?.salary + " DKK" : ""}
+              </Text>
+            </HStack>
+          </GridItem>
+        </Grid>
+      </Center>
+    </>
+  );
+}
 
 export default App;
